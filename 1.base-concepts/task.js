1d29e8b18ae4922f0baed9e3efa1aeabde289462
Tepokun <artememelyanow@gmail.com>
2023-06-12 11:00:16 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,13 +8,18 @@ function solveEquation(a, b, c) {
       let x = -b / (2 * a);
       arr.push(x);
     } else if (d > 0) {
-      let x = (-b + Math.sqrt(d) )/(2*a);
-      let y = (-b - Math.sqrt(d) )/(2*a);
+      let x = (-b + Math.sqrt(d) ) / (2 * a);
+      let y = (-b - Math.sqrt(d) ) / (2 * a);
       arr.push(x, y);
     }
   
   return arr;
 }
+
+let a = 1;
+let b = 3;
+let c = 3;
+console.log(solveEquation(a, b, c));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
