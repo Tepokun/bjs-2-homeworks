658c3eb93cafae795d8cb4b745b7f7c63ba76a0b
Tepokun <artememelyanow@gmail.com>
2023-06-12 17:38:29 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-    if (d < 0) {
-      return arr;
-    } else if (d === 0) {
+  if (d === 0) {
       let x = -b / (2 * a);
       arr.push(x);
     } else if (d > 0) {
