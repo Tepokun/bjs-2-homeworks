3c7e3340452f160924619386a7c5752029deaa76
Tepokun <artememelyanow@gmail.com>
2023-06-12 09:17:39 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4 * a * c;
+    if (d < 0) {
+      return arr;
+    } else if (d === 0) {
+      x = -b/(2*a);
+      arr = [x];
+    } else if (d > 0) {
+      x = (-b + Math.sqrt(d) )/(2*a);
+      y = (-b - Math.sqrt(d) )/(2*a);
+      arr = [x, y]
+    }
   
   return arr;
 }
