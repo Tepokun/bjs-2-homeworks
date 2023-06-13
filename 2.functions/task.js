c14536dad365480ec6980ffd502f2986a8472447
Tepokun <artememelyanow@gmail.com>
2023-06-13 16:19:14 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,3 +1,5 @@
+// Задача 1
+
 function getArrayParams(...arr) {
   let min = Infinity;
   let max = -Infinity;
@@ -19,6 +21,7 @@ function getArrayParams(...arr) {
   return { min, max, avg };
 }
 
+// Задача 2
 
 function summElementsWorker(...arr) {
 
@@ -97,21 +100,8 @@ function averageEvenElementsWorker(...arr) {
 
   return sumEvenElement / countEvenElement;
 }
-// summElementsWorker
-console.log(summElementsWorker()); // 0
-console.log(summElementsWorker(10, 10, 11, 20, 10)); // 61
 
-// differenceMaxMinWorker
-console.log(differenceMaxMinWorker()); // 0
-console.log(differenceMaxMinWorker(10, 10, 11, 20, 10)); // 20 - 10 => 10
-
-// differenceEvenOddWorker
-console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17)); // 266 - 213 => 53
-console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // 114 - 383 => -269
-
-// averageEvenElementsWorker
-console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
-console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
+//Задача 3
 
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
