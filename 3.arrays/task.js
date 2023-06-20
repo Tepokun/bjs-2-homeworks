12d9154ff225dd088a4f0e325112865f363cb448
Tepokun <artememelyanow@gmail.com>
2023-06-20 12:07:20 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,39 @@
+//Задача №1
+
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length !== arr2.length) {
+        return false;
+      }
+    
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+    
+      return true;
+    }
+
+//Задача №2
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(function(user) {
+        return user.gender === gender;
+    });
+    
+    const ages = filteredUsers.map(function(user) {
+        return user.age;
+    });
+    
+    const sum = ages.reduce(function(total, age) {
+        return total + age;
+    }, 0);
+    
+    const averageValue = sum / ages.length;
+    
+    if (isNaN(averageValue)) {
+        return 0;
+    } else {
+        return averageValue;
+    }
 }
