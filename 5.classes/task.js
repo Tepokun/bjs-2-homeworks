59c4ea7a70cf36fbc1c2ae2f2d9997f60e073a3b
Tepokun <artememelyanow@gmail.com>
2023-06-27 21:45:12 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,123 +1,81 @@
-// Задача 1. Печатное издание
-
-class Student {
-    constructor(name) {
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
       this.name = name;
-      this.marks = {};
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
     }
   
-    addMark(mark, subject) {
-      if (mark < 2 || mark > 5) {
-        return;
-      }
-  
-      if (!this.marks.hasOwnProperty(subject)) {
-        this.marks[subject] = [];
-      }
-  
-      this.marks[subject].push(mark);
+    fix() {
+      this.state *= 1.5;
     }
   
-    getAverageBySubject(subject) {
-      if (!this.marks.hasOwnProperty(subject)) {
-        return 0;
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
       }
-  
-      const marks = this.marks[subject];
-      const sum = marks.reduce((acc, mark) => acc + mark, 0);
-      return sum / marks.length;
     }
   
-    getAverage() {
-      const subjects = Object.keys(this.marks);
-      const totalSum = subjects.reduce((acc, subject) => {
-        const average = this.getAverageBySubject(subject);
-        return acc + average;
-      }, 0);
-  
-      return totalSum / subjects.length;
+    get state() {
+      return this._state;
     }
   }
   
-  // Задача 2 библиотека
+  const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  );
   
-  class Library {
-    constructor(name) {
-      this.name = name;
-      this.books = [];
-    }
+  console.log(sherlock.releaseDate); // 2019
+  console.log(sherlock.state); // 100
+  sherlock.fix();
+  console.log(sherlock.state); // 100
   
-    addBook(book) {
-      if (book.getState() > 30) {
-        this.books.push(book);
-      }
-    }
-  
-    findBookBy(type, value) {
-      for (const book of this.books) {
-        if (book[type] === value) {
-          return book;
-        }
-      }
-      return null;
-    }
-  
-    giveBookByName(bookName) {
-      for (let i = 0; i < this.books.length; i++) {
-        if (this.books[i].name === bookName) {
-          const book = this.books[i];
-          this.books.splice(i, 1);
-          return book;
-        }
-      }
-      return null;
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine";
     }
   }
   
-  // Задача 3 Журнал
-  
-  class Student {
-    constructor(name) {
-      this.name = name;
-      this.marks = {};
-    }
-  
-    addMark(mark, subject) {
-      if (mark < 2 || mark > 5) {
-        return;
-      }
-  
-      if (!this.marks.hasOwnProperty(subject)) {
-        this.marks[subject] = [];
-      }
-  
-      this.marks[subject].push(mark);
-    }
-  
-    getAverageBySubject(subject) {
-      if (!this.marks.hasOwnProperty(subject)) {
-        return 0;
-      }
-  
-      const marks = this.marks[subject];
-      const sum = marks.reduce((acc, mark) => acc + mark, 0);
-      return sum / marks.length;
-    }
-  
-    getAverage() {
-      const subjects = Object.keys(this.marks);
-      const totalSum = subjects.reduce((acc, subject) => {
-        const average = this.getAverageBySubject(subject);
-        return acc + average;
-      }, 0);
-  
-      return totalSum / subjects.length;
+  class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount);
+      this.type = "book";
+      this.author = author;
     }
   }
-
-  // добавление оценок по предметам
-  student.addMark(5, "химия");
-  student.addMark(5, "химия");
-  student.addMark(5, "физика");
-  student.addMark(4, "физика");
-  student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+  
+  class NovelBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount, author);
+      this.type = "novel";
+    }
+  }
+  
+  class FantasticBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount, author);
+      this.type = "fantastic";
+    }
+  }
+  
+  class DetectiveBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount, author);
+      this.type = "detective";
+    }
+  }
+  
+  const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
